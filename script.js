16fba87eba49f9c2f3ef218592da3a1ac6eceb5b
Vrushali Patil <113831357+Vrushali1041@users.noreply.github.com>
2023-04-12 13:32:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,36 @@
 //complete this code
-class Animal {}
+// class Animal {}
 
-class Dog extends Animal {}
+// class Dog extends Animal {}
 
-class Cat extends Animal {}
+// class Cat extends Animal {}
+
+class Animal {
+  constructor(species) {
+    this._species = species;
+  }
+
+  get species() {
+    return this._species;
+  }
+
+  makeSound() {
+    console.log("The " + this._species + " makes a sound.");
+  }
+}
+
+class Cat extends Animal {
+  purr() {
+    console.log("purr");
+  }
+}
+
+class Dog extends Animal {
+  bark() {
+    console.log("woof");
+  }
+}
+
 
 // Do not change the code below this line
 window.Animal = Animal;
